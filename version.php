7d7713589a59fa8af629d14876549fe5e236ed4a
Ivan Kolodrivskiy <kribetko@gmail.com>
2018-04-26 10:28:13 +0300

    fix version file

--- version.php
+++ version.php
@@ -20,17 +20,17 @@
  * Setting the $plugin->version to 0 prevents the plugin from being installed.
  * See https://docs.moodle.org/dev/version.php for more info.
  *
- * @package    mod_npumoodlestat
- * @copyright  2016 Your Name <your@email.address>
+ * @package    block_npumoodlestat
+ * @copyright  2016 Your Name <kribetk@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'mod_npumoodlestat';
-$plugin->version = 0;
-$plugin->release = 'v0.0';
+$plugin->component = 'block_npumoodlestat';
+$plugin->version = 20180421;
+$plugin->release = 'v0.1';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->cron = 0;
-$plugin->dependencies = array();
+$plugin->dependencies = [];
