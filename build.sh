19abab296aee495343632ec75606ace0a52ae12b
Ivan Kolodrivskiy <kribetko@gmail.com>
2018-05-03 10:22:25 +0300

    fix build script

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ module_name="npumoodlestat"
 build_dir="build"
 
 files_to_move=(
-"db"
+
 "lang"
 "index.php"
 "lib.php"
+"db"
 "locallib.php"
 "mod_form.php"
 "README.txt"
@@ -23,11 +24,4 @@ else
     rm -rfv ${build_dir}/*
 fi
 
-for entity in ${files_to_move[@]}
-do
-	cp -r ${entity} ${build_dir}/${module_name}
-done
-
-zip -r build/${module_name}_$(date '+%d%m%Y %H%M%S') ${build_dir}/${module_name}
-
-rm -rf ${build_dir}/${module_name}
+zip -r build/${module_name}_$(date '+%d%m%Y %H%M%S') ${files_to_move[@]}
